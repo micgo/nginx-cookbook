7e8db9f09f20fd3f2a5570efdfbe6d082bfcf5d6
Michael Goetz <mpgoetz@gmail.com>
2014-02-20 08:41:20 -0600

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'nginx-cookbook'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'mpgoetz@getchef.com'
+license          'All rights reserved'
+description      'Installs/Configures nginx-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: nginx-cookbook
+# Recipe:: default
+#
+# Copyright 2014, Chef Software, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+remote_file "#{Chef::Config[:file_cache_path]}/nginx.deb" do
+  owner "root"
+  group "root"
+  mode "0644"
+  source "http://ec2-54-80-160-92.compute-1.amazonaws.com:8080/job/nginx-app/ws/nginx_1.4.1-1_amd64.deb"
+  checksum "e2d6c544be6282daa148e2d1a0259309d94741a6803d797b63de521b7e0202f8"
+end
+
+package "nginx" do
+  source "#{Chef::Config[:file_cache_path]}/nginx.deb"
+  action :install
+end
