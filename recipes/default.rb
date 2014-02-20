5d5aa0936a5be5710d996dd2ab2007606cc104fb
Michael Goetz <mpgoetz@gmail.com>
2014-02-20 09:12:17 -0600

    Adding libpce3 dependency

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,10 @@ remote_file "#{Chef::Config[:file_cache_path]}/nginx.deb" do
   checksum "e2d6c544be6282daa148e2d1a0259309d94741a6803d797b63de521b7e0202f8"
 end
 
+package 'libpce3' do
+  action :install
+end
+
 dpkg_package "nginx" do
   source "#{Chef::Config[:file_cache_path]}/nginx.deb"
   action :install
